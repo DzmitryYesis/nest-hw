48196dcb5431cf24bb7f111f14c52fe2daa76804
Dzmitry Yesis <dzmitryyesis@gmail.com>
2025-09-10 15:08:37 +0200

    hw18

--- src/settings.ts
+++ src/settings.ts
@@ -5,7 +5,7 @@ export const SETTINGS = {
   REFRESH_TOKEN_NAME: 'refreshToken',
   JWT_ACCESS_TOKEN_SECRET: process.env.JWT_ACCESS_SECRET || 'qwerqwer',
   JWT_REFRESH_TOKEN_SECRET: process.env.JWT_REFRESH_SECRET || 'sfghsftbhsf',
-  JWT_ACCESS_TOKEN_EXPIRES_TIME: '10s',
+  JWT_ACCESS_TOKEN_EXPIRES_TIME: '5m',
   JWT_REFRESH_TOKEN_EXPIRES_TIME: '20s',
   AUTH_BASIC: 'admin:qwerty',
   MONGO_URL: 'mongodb://localhost:27017',
